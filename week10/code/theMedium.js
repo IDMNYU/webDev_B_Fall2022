e6508f85cc40e9065b1d48ce57b2dfeab65e7321
rebleo <rml444@nyu.edu>
2022-11-16 14:02:27 -0500

    muluhan lecture

--- week10/code/theMedium.js
+++ week10/code/theMedium.js
@@ -9,7 +9,7 @@ console.log(theUser + " " + theNumber)
 
 const theButton = document.querySelector('button')
 console.log(theButton)
-let theMain = document.querySelector('main')
+let theMain = document.querySelector('body')
 let theTxt = document.getElementById('theText')
 
 theButton.addEventListener('click', changeColor)
@@ -19,21 +19,39 @@ function changeColor(){
     console.log("horray!")
     theTxt.textContent = "you clicked the button!"
     theMain.style.backgroundColor = "yellow"
-    
-
 }
-let thePara;
+
+// let thePara;
+// let theGraphs = []
+// let theClicks = []
+function makeAll(){
+    console.log("made!");
+   let thePush =  document.createElement('button');
+   thePush.style.backgroundColor = "green"
+   // when user clicks button a new button is created
+   const theContent = document.createTextNode("rebecca")
+   thePush.appendChild(theContent)
+   document.body.appendChild(thePush)
+
+ 
+}
 
 function getReal(){
     console.log("ya real")
     //https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
-  
     thePara =  document.createElement("p");
    const theContent = document.createTextNode("what the waht?")
+   console.log(theContent)
     thePara.appendChild(theContent);
     const thisDiv = document.querySelector("div");
-    document.body.insertBefore(thePara,thisDiv)
+    document.body.insertBefore(thePara,thisDiv);
+
+    for (let i =0; i < 10; i++){
+        console.log(i)
+     
+    }
+    console.log(theGraphs)
 }
 
-document.getElementById('fire').addEventListener('click', getReal)
+document.getElementById('fire').addEventListener('click', makeAll)
 
